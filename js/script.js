34623fd75b7ca3ee2674258216f1e57f68f95bed
mkspcd <mkspcd@gmail.com>
2018-08-25 17:13:35 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      quote: String,
+      author: String,
+      quotes: Array,
+      dropboxURI: 'https://dl.dropboxusercontent.com/s/5kminh6mr9wnhe6/quotes.json?dl=1',
+      githubURI: 'https://raw.githubusercontent.com/mkspcd/DeveloperExcuses/master/quotes.json'
+    }
+  },
+  created() {
+    fetch(this.dropboxURI)
+      .then(response => response.json())
+      .then(source => {
+        this.init(source.quotes)
+      })
+      .catch(() => {
+        fetch(this.githubRequest)
+          .then(response => response.json())
+          .then(source => {
+            this.init(source.quotes)
+          })
+          .catch(() => {
+            this.init(['404'])
+          })
+      })
+  },
+  methods: {
+    init(data) {
+      this.quotes = data
+      this.randomQuote()
+    },
+    randomQuote() {
+      const randomIndex = Math.floor(Math.random() * this.quotes.length)
+
+      this.quote = this.quotes[randomIndex].text
+      this.author =
+        this.quotes[randomIndex].author !== '' ?
+        this.quotes[randomIndex].author :
+        'Anonyme'
+    },
+    addQuotationMarks: quote => '« ' + quote + ' »'
+  }
+})
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Developer Excuses
+A collection of good excuses for developers.  
+
+All excuses have been heard or used in a working environment.  
+
+Go to the live [**website here**](https://mkspcd.github.io/DeveloperExcuses/).  
+  
+<p align="center">
+  <img src="https://raw.githubusercontent.com/mkspcd/DeveloperExcuses/master/screenshot.png" alt="Screenshot" />
+</p>
+
+## Disclaimer
+
+​​All the excuses provided on this website are provided “as is” and you agree that you use such excuses entirely at your own risk.
